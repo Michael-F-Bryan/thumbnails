a2de863c56dd59c64b740b3abc099c0bf6336668
Michael-F-Bryan <michaelfbryan@gmail.com>
2020-06-05 03:36:39 +0800

    Updated the #[cfg] so `cargo doc` will run on Linux again

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -3,7 +3,7 @@
 macro_rules! gated {
     ($( #[cfg($cfg:meta)] $item:item )*) => {
         $(
-            #[cfg_attr(docsrs, cfg(any($cfg, doc)), doc(cfg($cfg)))]
+            #[cfg_attr(docsrs, cfg($cfg), doc(cfg($cfg)))]
             #[cfg_attr(not(docsrs), cfg($cfg))]
             $item
         )*
